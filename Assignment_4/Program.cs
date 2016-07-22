552a1a43d992e19da6ef83da953b08cbbca04654
Fasecity <Audiotrash112@gmail.com>
2016-07-22 14:36:09 -0400

    commit

--- Assignment_4/Program.cs
+++ Assignment_4/Program.cs
@@ -1,15 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+/// <summary>
+/// Name: Mohamoud Mohamed
+/// student#:300435435
+/// Assigment 4 part B
+/// version 1
+/// </summary>
 namespace Assignment_4
 {
     class Program
-    {
+    {   /// <summary>
+    /// Main method
+    /// </summary>
+    /// <param name="args"></param>
         static void Main(string[] args)
         {
+
+            List<string> seating = new List<string>();
+            for (int index = 0; index < 10; index++)
+            {
+                seating.Add(index <= 4 ? "Economy" : "First Class");
+            }
+            List<string> assignedSeat = new List<string>();
+
+            DisplayMenu(seating, assignedSeat);
+
+        }
+        /// <summary>
+        /// this method shows menu
+        /// </summary>
+        /// <param name="seating"></param>
+        /// <param name="assignedSeat"></param>
+        public static void DisplayMenu(List<string> seating, List<string> assignedSeat)
+        {
+            bool loop = true;
+            while (loop)
+            {
+                Console.WriteLine("1. First Class");
+
+                Console.WriteLine("2. Economy");
+
+                Console.WriteLine("3. Exit");
+
+                Console.WriteLine("Please Select Your option: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        FirstClass(seating, assignedSeat);
+                        break;
+                    case 2:
+                        Economy(seating, assignedSeat);
+                        break;
+                    case 3:
+                        loop = false;
+                        break;
+
+
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// This method adds customer to first Class
+        /// </summary>
+        /// <param name="seats"></param>
+        /// <param name="assigned"></param>
+        public static void FirstClass(List<string> seats, List<string> assigned)
+        {
+            if (!seats.Remove("First Class"))
+            {
+                Console.WriteLine("Is it alright to be in the economy section?");
+                string answer = Console.ReadLine();
+
+                if (answer != "yes")
+                    Console.WriteLine("Sorry your next flight leaves in a few hours");
+                else
+                    Economy(seats, assigned);
+            }
+            else
+            {
+                assigned.Add("First Class");
+                Console.WriteLine("You have been added to first class");
+            }
+        }
+
+        /// <summary>
+        /// This method adds the customer to economy
+        /// </summary>
+        /// <param name="seat"></param>
+        /// <param name="assigned"></param>
+        public static void Economy(List<string> seat, List<string> assigned)
+        {
+            if (seat.Remove("Economy"))
+            {
+                assigned.Add("Economy");
+                Console.WriteLine("You have been added to economy class");
+            }
+            else
+            {
+                Console.WriteLine("Is it okay to be placed in the First Class section");
+                string answer = Console.ReadLine();
+
+                if (answer == "yes")
+                {
+                    FirstClass(seat, assigned);
+                }
+                else
+                {
+                    Console.WriteLine("Sorry but your next flight will leave in a few hours");
+                }
+            }
         }
     }
 }
---
+++ Dice/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+/// <summary>
+/// Name: Mohamoud Mohamed
+/// student#:300435435
+/// Assigment 4 part A
+/// version 1
+/// </summary>
+namespace Dice
+{
+    class Program
+    {   /// <summary>
+    /// Main method
+    /// </summary>
+    /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Dice();
+
+        }
+        /// <summary>
+        /// Die roll method
+        /// </summary>
+        public static void Dice()
+        {
+
+            int[] tally = new int[11];
+
+            Random rnd = new Random();
+            for (int index = 0; index <= 36000; index++)
+            {
+                int diceOne = rnd.Next(1, 7);
+                int diceTwo = rnd.Next(1, 7);
+                int sumOfDie = diceOne + diceTwo;
+
+                tally[sumOfDie - 2]++;
+            }
+            Console.WriteLine(" ===============DICE ROLLER=================");
+            Console.WriteLine($"| {"Sum",5} | {"times die are Rolled",-23}|");
+            Console.WriteLine(" ===========================================");
+            for (int counter = 0; counter < tally.Length; counter++)
+            {
+                Console.WriteLine($"| {counter + 2,5} | {tally[counter],-23}|");
+                Console.WriteLine(" ---------------------------------------");
+
+
+            }
+        }
+
+    }
+}
